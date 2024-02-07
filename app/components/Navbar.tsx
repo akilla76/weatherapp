aa17bfdc3b5b1d63a064b18050bae5675af6c634
Andrey <akilla76@users.noreply.github.com>
2024-02-07 17:06:36 -0500

    a1

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,1 +1,17 @@
-tsrfc
+import * as React from 'react';
+
+interface INavBarProps {
+}
+
+export function NavBar (props: INavBarProps) {
+    return (
+        <nav className="shadow-sm sticky top-0 left-0 z-50 bg-white">
+            <div className="h-[80px] w-full flex justify-between items-center max-w-7xl px-3 mx-auto">
+                <p className="flex items-center justify-center gap-2"></p>
+            </div>Navbar
+
+        </nav>
+      
+    );
+}
+
